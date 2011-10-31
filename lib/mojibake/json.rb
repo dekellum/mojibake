e62fbc20ef28c3c08166b7287ae7475ac9636510
David Kellum <dek-oss@gravitext.com>
2011-10-30 17:02:44 -0700

    Use single UNICODE escapes in JSON output, throughout

--- lib/mojibake/json.rb
+++ lib/mojibake/json.rb
@@ -23,15 +23,35 @@ module MojiBake
 
     # table as self contained json-ready Hash
     def json_object
+
+      # Also use unicode excape for the "interesting" subset of moji
+      # mappings.
+      moji = hash.sort.map do |kv|
+        kv.map do |s|
+          s.codepoints.inject( '' ) do |r,i|
+            if INTEREST_CODEPOINTS.include?( i )
+              r << sprintf( '\u%04X', i )
+            else
+              r << i.chr( Encoding::UTF_8 )
+            end
+          end
+        end
+      end
+
       { :mojibake => MojiBake::VERSION,
         :url => "https://github.com/dekellum/mojibake",
         :regexp => regexp.inspect[1...-1],
-        :moji => Hash[ hash.sort ] }
+        :moji =>  Hash[ moji ] }
     end
 
     # Pretty formatted JSON serialized String for json_object
     def json
-      JSON.pretty_generate( json_object )
+      # Generate and replace what become double escaped '\\u' UNICODE
+      # escapes with '\u' escapes. This is a hack but is reasonably
+      # safe given that 'u' isn't normally escaped.  The alterantive
+      # would be to hack JSON package or do the JSON formatting
+      # ourselves.
+      JSON.pretty_generate( json_object ).gsub( /\\\\u/, '\u' )
     end
 
   end
